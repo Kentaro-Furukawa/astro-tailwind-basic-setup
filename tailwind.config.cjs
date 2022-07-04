fdb7f9214677ef5c7f6b0b59c1dda1863ba9802a
Ken <info@ken89.com>
2022-07-04 10:41:45 +0900

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	mode: 'jit',
-	purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
-	// content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
